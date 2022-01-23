70fe10d45aa51708e55097431ce2a8645fb33a68
YUVARAJ K <96658030+Yuvaraj710@users.noreply.github.com>
2022-01-23 20:47:28 +0530

    Add files via upload

---
+++ Week 3/Day 1/Assignment 1/Computer.java
@@ -0,0 +1,12 @@
+package week3.day1.assignment;
+
+public class Computer {
+
+	public void computer() {
+		System.out.println("This is a Personal Computer");
+	}
+	
+	public void Laptop() {
+		System.out.println("The laptop model is Asus");
+	}
+}
---
+++ Week 3/Day 1/Assignment 1/Desktop.java
@@ -0,0 +1,14 @@
+package week3.day1.assignment;
+
+public class Desktop extends Computer {
+
+	public static void main(String[] args) {
+		Computer model = new Computer();
+		
+		model.computer();
+		model.Laptop();
+		
+
+	}
+
+}
